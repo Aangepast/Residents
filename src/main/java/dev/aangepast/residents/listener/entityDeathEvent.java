8ec261ab2760afbefb2a0c539b6b1f87fd308afe
Aangepast <tobiaslbeumer@gmail.com>
2023-07-06 11:00:18 +0200

    v1.0.1

--- src/main/java/dev/aangepast/residents/listener/entityDeathEvent.java
+++ src/main/java/dev/aangepast/residents/listener/entityDeathEvent.java
@@ -7,6 +7,8 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.inventory.InventoryInteractEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class entityDeathEvent implements Listener {
